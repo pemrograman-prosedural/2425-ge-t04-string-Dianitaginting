41059b55809050ff28a0bb32a034d2f30a536294
Dianitaginting <dianitaginting06@gmail.com>
2025-02-27 14:19:21 +0700

    PAIR 9 SELESAI

--- t04_02.c
+++ t04_02.c
@@ -5,19 +5,20 @@
 #include <string.h>
 
 int main(int _argv, char **_argc){
-    char input[21]; 
-    fgets(input, sizeof(input), stdin);
-
-    
-    size_t len = strlen(input);
-    if (len > 0 && input[len - 1] == '\n') {
-        input[len - 1] = '\0';
-    }
-
-    for (int i = 0; i < strlen(input); i++) {
-        printf("%03d", (int)input[i]); 
-    }
-    printf("\n");
-
-    return 0;
+      char input[21];
+      fgets(input, sizeof(input), stdin);
+  
+      
+      size_t len = strlen(input);
+      if (len > 0 && input[len - 1] == '\n') {
+          input[len - 1] = '\0';
+      }
+  
+      
+      for (int i = 0; i < strlen(input); i++) {
+          printf("%03d", (int)input[i]); 
+      }
+      printf("013\n"); 
+  
+      return 0;
 }
