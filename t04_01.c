69718cd5ea3eed24d597c2361a49d57032747da1
Dianitaginting <dianitaginting06@gmail.com>
2025-02-27 14:08:57 +0700

    PAIR 9 SELESAI

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,31 @@
-// NIM - Name
-// NIM - Name
+// 12S24015 - Lucas Pardede
+// 12S24044 - Dianita Lorensia Br Ginting
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
 
 int main(int _argv, char **_argc) {
-  // codes
+    char input[61]; 
+    scanf("%s", input);
 
-  return 0;
+    int length = strlen(input);
+    if (length < 3 || length > 60 || length % 3 != 0) {
+        printf("Input tidak valid.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < length; i += 3) {
+        char ascii_str[4];
+        ascii_str[0] = input[i];
+        ascii_str[1] = input[i+1];
+        ascii_str[2] = input[i+2];
+        ascii_str[3] = '\0';
+
+        int ascii_code = atoi(ascii_str); 
+        printf("%c", (char)ascii_code); 
+    }
+    printf("\n");
+
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,23 @@
-// NIM - Name
-// NIM - Name
+// 12S24015 - Lucas Pardede
+// 12S24044 - Dianita Lorensia Br Ginting
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main(int _argv, char **_argc){
+    char input[21]; 
+    fgets(input, sizeof(input), stdin);
 
-  return 0;
+    
+    size_t len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n') {
+        input[len - 1] = '\0';
+    }
+
+    for (int i = 0; i < strlen(input); i++) {
+        printf("%03d", (int)input[i]); 
+    }
+    printf("\n");
+
+    return 0;
 }
